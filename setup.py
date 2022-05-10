f19e65b8721a6157a9fa203e30695c21af3c096c
Doğukan Kurnaz <dogukanertunga@gmail.com>
2022-05-10 14:13:45 +0300

    ci/cd

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="JSON-Parser",
+    author="Dogukan Ertunga Kurnaz",
+    version="1.0.0",
+    description="A little helper tool to split big json files into chunks",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    url="https://github.com/DogukanK/JSON-Chunker",
+    python_requires=">=3.6",
+
+)
---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+from src.chunker import getDefinition
